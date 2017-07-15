ff802f75667e53e2fa4e04a6e52b88fbce4a4e84
Florian Hatat <fhatat@free.fr>
2017-07-15 18:10:26 +0200

    base: ajout des vues d'identification

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
-	url(r'^login/', views.login, name='login'),
+	url(r'^', include('django.contrib.auth.urls')),
 ]
--- base/views/__init__.py
+++ base/views/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .login import login
 from .home import home

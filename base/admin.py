da7b64a659ef77e5ea0ac65ec9799c14e354b101
Florian Hatat <fhatat@free.fr>
2017-07-23 13:11:31 +0200

    Ajout du site d'administration

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Professeur, Etudiant
+from .models import Academie, Annee, Etablissement
 
-# Register your models here.
+class PykolAdminSite(AdminSite):
+	site_header = 'Administration de pyKol'
+	site_title = 'Administration de pyKol'
+admin_site = PykolAdminSite(name="pykol_admin")
+
+class ProfesseurAdmin():
+	pass
+admin_site.register(User, UserAdmin)
+admin_site.register(Professeur, UserAdmin)
+admin_site.register(Etudiant, UserAdmin)
+
+admin_site.register(Etablissement)
+admin_site.register(Annee)
+
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class AcademieResource(resources.ModelResource):
+	class Meta:
+		model = Academie
+class AcademieAdmin(ImportExportModelAdmin):
+	    resource_class = AcademieResource
+admin_site.register(Academie, AcademieAdmin)
--- colles/admin.py
+++ colles/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from base.admin import admin_site
+from .models import Semaine, Creneau, Colle
 
-# Register your models here.
+admin_site.register(Semaine)
+admin_site.register(Creneau)
+admin_site.register(Colle)

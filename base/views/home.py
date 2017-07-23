13b694460e0efb67818a32a1e08e0c25b0434ff3
Florian Hatat <fhatat@free.fr>
2017-07-23 13:13:29 +0200

    Vues identification et accueil

--- base/views/home.py
+++ base/views/home.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8
 
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
-	pass
+	return render(request, 'base.html')
 

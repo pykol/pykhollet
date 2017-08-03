0ff20e2f7cadbffb75b84cd79ededafc1a84e0c5
Florian Hatat <pykol@hatat.me>
2017-08-03 15:07:00 +0200

    colles: modèles gérant les modifications de planning

--- colles/models/colles.py
+++ colles/models/colles.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 from django.db import models
 
-from base.models.enseignement import Classe, Professeur, Matiere, Etudiant
+from base.models.enseignement import Classe, Professeur, Matiere, Etudiant, Groupe
 from base.notes import NoteField
 from .conception import Creneau
 
@@ -10,24 +10,34 @@ LISTE_JOURS = enumerate(["lundi", "mardi", "mercredi", "jeudi",
 	"vendredi", "samedi", "dimanche"], 1)
 
 class Colle(models.Model):
-	creneau = models.ForeignKey(Creneau, null=True,
+	creneau = models.ForeignKey(Creneau, blank=True, null=True,
 			on_delete=models.SET_NULL)
-	horaire = models.DateTimeField()
-	colleur = models.ForeignKey(Professeur)
-	matiere = models.ForeignKey(Matiere)
-	classe = models.ForeignKey(Classe)
+	classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
 
-	ETATS_COLLE = enumerate([
+	ETATS = enumerate([
 		"Prévue", "Notation en attente", "Notée", "Relevée", "Annulée"
 		])
 	etat = models.PositiveSmallIntegerField(verbose_name="état",
-			choices=ETATS_COLLE)
+			choices=ETATS)
+
+class ColleDetails(models.Model):
+	colle = models.ForeignKey(Colle, on_delete=models.CASCADE)
+	horaire = models.DateTimeField()
+	salle = models.CharField(max_length=30)
+	colleur = models.ForeignKey(Professeur, blank=True, null=True,
+			on_delete=models.SET_NULL)
+	matiere = models.ForeignKey(Matiere, blank=True, null=True,
+			on_delete=models.SET_NULL)
+	groupe = models.ForeignKey(Groupe, blank=True, null=True,
+			on_delete=models.SET_NULL)
+	eleves = models.ManyToManyField(Etudiant, blank=True)
+	actif = models.BooleanField()
 
 class ColleNote(models.Model):
-	colle = models.ForeignKey(Colle)
+	colle = models.ForeignKey(Colle, on_delete=models.CASCADE)
 	eleve = models.ForeignKey(Etudiant)
 	note = NoteField()
-	horaire = models.DateField()
+	horaire = models.DateTimeField()
 
 class ColleReleve(models.Model):
-	colles = models.ManyToManyField(Colle)
+	colles = models.ManyToManyField(Colle, blank=True)

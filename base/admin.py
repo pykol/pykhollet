d8284175af6945e9b35aff383b6c87a7994d60eb
Florian Hatat <pykol@hatat.me>
2017-08-13 14:53:10 +0200

    admin: décorateur @register

--- base/admin.py
+++ base/admin.py
@@ -2,16 +2,20 @@
 from __future__ import unicode_literals
 
 from django.utils.translation import gettext, gettext_lazy as _
-from django.contrib.admin import AdminSite
+from django.contrib import admin
+
 from .models import User, Professeur, Etudiant
 from .models import Academie, Annee, Etablissement
 from .models import Matiere, Classe
 
-class PykolAdminSite(AdminSite):
+class PykolAdminSite(admin.AdminSite):
 	site_header = 'Administration de pyKol'
 	site_title = 'Administration de pyKol'
 admin_site = PykolAdminSite(name="pykol_admin")
 
+def register(*models, **kwargs):
+	kwargs['site'] = admin_site
+	return admin.register(*models, **kwargs)
 
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
@@ -19,6 +23,8 @@ class PykolUserCreateForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ('username', 'sexe', 'email',)
+
+@register(User)
 class PykolUserAdmin(UserAdmin):
 	add_form = PykolUserCreateForm
 	add_fieldsets = ((None, {
@@ -37,6 +43,8 @@ class ProfesseurCreateForm(PykolUserCreateForm):
 	class Meta:
 		model = Professeur
 		fields = ('username', 'sexe', 'email', 'corps', 'etablissement')
+
+@register(Professeur)
 class ProfesseurAdmin(PykolUserAdmin):
 	add_form = ProfesseurCreateForm
 	add_fieldsets = ((None, {
@@ -57,6 +65,8 @@ class EtudiantCreateForm(PykolUserCreateForm):
 	class Meta:
 		model = Etudiant
 		fields = ('username', 'sexe', 'email', 'ine', 'classe')
+
+@register(Etudiant)
 class EtudiantAdmin(PykolUserAdmin):
 	add_form = EtudiantCreateForm
 	add_fieldsets = ((None, {
@@ -75,11 +85,6 @@ class EtudiantAdmin(PykolUserAdmin):
 			'entree', 'sortie')}),
 		)
 
-admin_site.register(User, PykolUserAdmin)
-admin_site.register(Professeur, ProfesseurAdmin)
-admin_site.register(Etudiant, EtudiantAdmin)
-
-
 admin_site.register(Etablissement)
 admin_site.register(Annee)
 
@@ -90,9 +95,10 @@ from import_export.admin import ImportExportModelAdmin
 class AcademieResource(resources.ModelResource):
 	class Meta:
 		model = Academie
+
+@register(Academie)
 class AcademieAdmin(ImportExportModelAdmin):
 	resource_class = AcademieResource
-admin_site.register(Academie, AcademieAdmin)
 
 admin_site.register(Matiere)
 admin_site.register(Classe)

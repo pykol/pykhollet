1cb67c6807503a74d44fba8f16cac1f07326ad1c
Florian Hatat <pykol@hatat.me>
2018-06-29 07:26:18 +0200

    Ébauche de modèle pour suivre la dotation en heures

---
+++ pykol/models/colles/dotation.py
@@ -0,0 +1,28 @@
+# -*- coding:utf8 -*-
+
+# pyKol - Gestion de colles en CPGE
+# Copyright (c) 2018 Florian Hatat
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from django.db import models
+
+from pykol.models.base import Annee
+
+class Dotation(models.Model):
+	"""Délégation d'heures de colles par le rectorat"""
+
+	annee = models.ForeignKey(Annee, on_delete=models.CASCADE)
+	date = models.DateField()
+	heures = models.IntegerField(verbose_name="nombre d'heures")

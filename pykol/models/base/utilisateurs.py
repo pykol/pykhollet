eaa71303d2c673d53fe62b11c05e0989287da0bd
Florian Hatat <pykol@hatat.me>
2018-07-01 11:43:38 +0200

    Garder la valeur None pour l'email dans User
    Les e-mails vides sont stockés avec la valeur NULL dans la base de
    données afin de ne pas violer la contrainte d'unicité.

--- pykol/models/base/utilisateurs.py
+++ pykol/models/base/utilisateurs.py
@@ -27,37 +27,37 @@ from pykol.models.fields import Lettre23Field
 from pykol.models.base import Etablissement
 
 class UserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
+	"""Define a model manager for User model with no username field."""
 
-    use_in_migrations = True
+	use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
-        """Create and save a User with the given email and password."""
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+	def _create_user(self, email, password, **extra_fields):
+	    """Create and save a User with the given email and password."""
+	    if not email:
+	        raise ValueError('The given email must be set')
+	    email = self.normalize_email(email)
+	    user = self.model(email=email, **extra_fields)
+	    user.set_password(password)
+	    user.save(using=self._db)
+	    return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+	def create_user(self, email, password=None, **extra_fields):
+	    """Create and save a regular User with the given email and password."""
+	    extra_fields.setdefault('is_staff', False)
+	    extra_fields.setdefault('is_superuser', False)
+	    return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+	def create_superuser(self, email, password, **extra_fields):
+	    """Create and save a SuperUser with the given email and password."""
+	    extra_fields.setdefault('is_staff', True)
+	    extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+	    if extra_fields.get('is_staff') is not True:
+	        raise ValueError('Superuser must have is_staff=True.')
+	    if extra_fields.get('is_superuser') is not True:
+	        raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+	    return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
 	"""
@@ -97,6 +97,23 @@ class User(AbstractUser):
 				return 'Mme'
 			else:
 				return 'Madame'
+
+	# On redéfinit cette méthode, présente dans BaseUserManager, car
+	# celle d'origine ne prend pas en compte l'option null=True sur le
+	# champ email : elle renvoie '' au lieu de None.
+	@classmethod
+	def normalize_email(cls, email):
+	    """
+	    Normalize the email address by lowercasing the domain part of it.
+	    """
+	    email = email or None
+	    try:
+	        email_name, domain_part = email.strip().rsplit('@', 1)
+	    except ValueError:
+	        pass
+	    else:
+	        email = email_name + '@' + domain_part.lower()
+	    return email
 
 	def __str__(self):
 		nom = '{last} {first}'.format(last=self.last_name.upper(),

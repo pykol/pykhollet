5a0b6635bf72b607ec68cb6d2882e38e52833766
Florian Hatat <pykol@hatat.me>
2018-07-13 22:03:15 +0200

    Typo dans le nom du projet dans wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pykol project.
+WSGI config for pykhollet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pykol.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pykhollet.settings")
 
 application = get_wsgi_application()

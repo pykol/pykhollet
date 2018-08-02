2f985232baae65affd90a0891c54c6490cdef961
Florian Hatat <pykol@hatat.me>
2018-08-02 12:19:59 +0200

    Gabarits spécifiques à pyKhollet dans le répertoire projet

--- pykhollet/settings.py
+++ pykhollet/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'pykhollet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'pykhollet/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

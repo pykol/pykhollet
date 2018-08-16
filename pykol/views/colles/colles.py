11ed05ea06ab900d43667625dba27f144d6ec3cb
Florian Hatat <pykol@hatat.me>
2018-08-16 21:27:03 +0200

    Déclaration d'une colle faite uniquement avec une requête POST

--- pykol/views/colles/colles.py
+++ pykol/views/colles/colles.py
@@ -96,8 +96,9 @@ def colle_declarer(request, pk):
 	# Les colles sous la forme de TD n'ont pas de notes, il suffit de
 	# confirmer qu'elles ont été réalisées
 	if colle.mode == Colle.MODE_TD:
-		colle.etat = Colle.ETAT_EFFECTUEE
-		colle.save()
+		if request.method == 'POST':
+			colle.etat = Colle.ETAT_EFFECTUEE
+			colle.save()
 		return redirect('colle_detail', colle.pk)
 
 	# On peuple le formulaire avec les élèves qui n'ont pas encore été

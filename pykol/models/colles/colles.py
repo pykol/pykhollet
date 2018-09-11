4c17bcddda4531ad1712512d4281a11173be9726
Florian Hatat <pykol@hatat.me>
2018-09-11 10:30:33 +0200

    Colle: fix import de datetime

--- pykol/models/colles/colles.py
+++ pykol/models/colles/colles.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from django.db import models, transaction
 from django.urls import reverse
